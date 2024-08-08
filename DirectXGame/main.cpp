2a43fc1d4614e94961338f8e5c59ee978b8f94e3
TsukadaHaruto <146901202+TsukadaHaruto@users.noreply.github.com>
2024-08-08 13:01:37 +0900

    0808

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"GC2D_01_アオキ_コウジ_AL3" );
+	win->CreateGameWindow(L"AL3_2-7" );
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
